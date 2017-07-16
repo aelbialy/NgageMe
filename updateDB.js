f60b2d213bf581672b9f025324b888cd6f7c3960
Kathryn Newbould <kathrynnewbould@yahoo.com>
2017-07-16 11:23:33 +0100

    Made observer form more mobile friendly
    Removed ability to update database with no user name or id, made buttons
    larger

--- updateDB.js
+++ updateDB.js
@@ -1,4 +1,4 @@
-
+function init(){
  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyD0xyFmePkcQCXYl-5kY8QOBFabJgtWl8g",
@@ -14,41 +14,54 @@
 document.getElementById('1-score').addEventListener('click',function(){
     var userID = document.getElementById('userID').value;
     var userName = document.getElementById('name').value;
+	if(userID && userName){
     var user1DataRef = firebase.database().ref(userID +'/');
     user1DataRef.update({name: userName, engagement: 1});
+	}
 });
 
 document.getElementById('2-score').addEventListener('click',function(){
     var userID = document.getElementById('userID').value;
     var userName = document.getElementById('name').value;
+    	if(userID && userName){
     var user1DataRef = firebase.database().ref(userID +'/');
     user1DataRef.update({name: userName, engagement: 2});
+	}
 });
 
 document.getElementById('3-score').addEventListener('click',function(){
     var userID = document.getElementById('userID').value;
     var userName = document.getElementById('name').value;
+	if(userID && userName){
     var user1DataRef = firebase.database().ref(userID +'/');
     user1DataRef.update({name: userName, engagement: 3});
+	}
 });
 
 document.getElementById('4-score').addEventListener('click',function(){
     var userID = document.getElementById('userID').value;
     var userName = document.getElementById('name').value;
+    	if(userID && userName){
     var user1DataRef = firebase.database().ref(userID +'/');
     user1DataRef.update({name: userName, engagement: 4});
+	}
 });
 
 document.getElementById('5-score').addEventListener('click',function(){
     var userID = document.getElementById('userID').value;
     var userName = document.getElementById('name').value;
+    	if(userID && userName){
     var user1DataRef = firebase.database().ref(userID +'/');
     user1DataRef.update({name: userName, engagement: 5});
+	}
 });
 
 document.getElementById('absentButton').addEventListener('click',function(){
     var userID = document.getElementById('userID').value;
     var userName = document.getElementById('name').value;
+	if(userID && userName){
     var user1DataRef = firebase.database().ref(userID +'/');
     user1DataRef.update({name: userName, engagement: 0});
+	}
 });
+}
