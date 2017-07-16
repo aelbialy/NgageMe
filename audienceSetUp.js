0fc7699d953576dcdc4d525796cc9b0901ebd52b
Kathryn Newbould <kathrynnewbould@yahoo.com>
2017-07-16 01:22:32 +0100

    Created working chart which updates based on incoming data from the students array.

--- audienceSetUp.js
+++ audienceSetUp.js
@@ -3,7 +3,7 @@ function displayClass(){
 			for(let i=0; i<classSize; i++){
 				var button = document.createElement("button");
 				button.setAttribute("id", students[i].id);
-				var body = document.getElementById("sidebar");
+				var body = document.getElementById("audienceIndicator");
 				button.style.backgroundColor = engagementColours[students[i].engagement-1];
 				button.innerHTML = students[i].name;
 				body.appendChild(button);
@@ -42,7 +42,7 @@ function timeStep(){
 //for demo only
 function randomiseEngagement(){
 	for(let i=0; i<classSize; i++){
-		students[i].engagement = Math.floor(5*Math.random());
+		students[i].engagement = Math.floor(5*Math.random()) + 1;
 	}
 }
 
@@ -50,4 +50,46 @@ function createStudents(){
 	for(let i = 0; i<15; i++){
 		students.push({id: i, name: "Student " + (i+1), engagement: 1});
 	}
+}
+function generateTimeAxis(){
+			for(let i = 0; i<31; i++){
+				timeAxis.push(i);
+			}
+}
+
+function chartMe() {
+		timeAxis = [];
+		generateTimeAxis();
+		let avgDataPoints = [];
+		
+		for(let i = 0; i<timeAxis.length; i++){
+			avgDataPoints.push({x: i+1, y:engagementAvg[i]?engagementAvg[i]:null});
+		}
+    var chart = new CanvasJS.Chart("content",
+    {
+      theme: "theme9",
+      title:{
+        text: ""
+      },
+      axisX: {
+        interval:3,
+		includeZero:true
+      },
+      axisY:{
+        includeZero: false,
+		maximum: 5,
+		minimum: 1,
+        interval:1
+      },
+      data: [
+      {        
+        type: "spline", 
+		color: 	"#1abc9c",
+		markerType: "none",
+        dataPoints: avgDataPoints
+      }
+      ]	
+    });
+
+	chart.render();
 }
